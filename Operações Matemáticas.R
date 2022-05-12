2574acd44540fe8919ead5a29bdb04410c17f201
Yas190 <ypires656@gmail.com>
2022-05-12 10:51:16 -0300

    updated

---
+++ Operações Matemáticas.R
@@ -0,0 +1,55 @@
+# Escreva seu código aqui
+# Para rodar seu código, selecione uma linha ou um cojunto de linhas e selecione 'Run' 
+# ou pressione ctrl + enter
+
+# OPERADORES MATEMÁTICOS
+# Soma
+5 + 4
+# Subtração
+10 - 8
+# Divisão
+20 / 2
+# Multiplicação
+40 * 2
+# Potência
+2 ** 3 #ou
+2 ^ 3
+# Raíz
+sqrt(121)
+# Exponencial
+exp(0)
+# log na base e
+log(1)
+# log na base 10
+log10(1)
+# log na base 2
+log2(4)
+# log na base 3 ou qualquer outra
+log(9, 3)
+# É igual?
+5 == 6
+5 == 5
+# É diferente?
+3 != 3
+3 != 6
+# É maior?
+35 > 20
+35 > 50
+# É menor?
+45 < 60
+45 < 32
+# É maior ou igual?
+4 >= 4
+4 >= 5
+# É menor ou igual?
+6 <= 8
+6 <= 5
+# E
+6 == 6 & 7 == 8
+6 == 6 & 7 != 8
+# OU 
+6 == 6 | 7 == 8
+6 != 6 | 7 == 8
+# Negação
+!6 == 6
+!4 > 2
