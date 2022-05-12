e304f0df81a7187a9dd88cc74d4cf5bae7bcaa72
Yas190 <ypires656@gmail.com>
2022-05-12 11:31:29 -0300

    updated

---
+++ Vetores.R
@@ -0,0 +1,20 @@
+# Para usar um vetor, usamos a FUNÇÃO c(). Para usar uma função no R, basta
+# saber o nome e os ARGUMENTOS que serão passados nos parênteses.
+# No caso do vetor, os argumentos são letra ou números.
+
+
+c(7,4, 1) # criando um vetor com 3 números
+
+c('sete', 'quatro', 'um') # criando um vetor com 3 nomes (vetor de caracteres)
+                          # os caracteres precisam estar entre aspas
+
+
+# Mas por que esse '[1]' antes dos elementos? Ele indica a POSIÇÃO 
+# do objeto seguinte
+
+5:35 # criando sequência de inteiros entre 5 e 35
+# a resposta mostra o '[1]' antes do 5 e '[21]' antes do 25, ou seja, o 5 está na 
+# 1° posição e o 25 na 21° posição.
+
+
+#
